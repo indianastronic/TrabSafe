b57646339585530aab9aa17ca9cdd1d8cd83d41b
indianastronic <guilherme.souza.santos.janutt@gmail.com>
2026-01-18 10:44:57 -0300

    fix: rotas frontend render

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 /* Garantir index */
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
